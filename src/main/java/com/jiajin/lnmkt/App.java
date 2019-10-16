c0f4d52654f2569e938687f08e9bd9e4347ad7b7
tj <18827529536@163.com>
2019-10-16 10:11:20 +0800

    添加日志xml

--- src/main/java/com/jiajin/lnmkt/App.java
+++ src/main/java/com/jiajin/lnmkt/App.java
@@ -1,14 +1,22 @@
 package com.jiajin.lnmkt;
 
+import java.util.Arrays;
+
 import org.activiti.spring.boot.SecurityAutoConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class App {
     
+    public static final Logger log = LoggerFactory.getLogger(App.class);
+    
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        ApplicationContext context = SpringApplication.run(App.class, args);
+        log.info("spring started. profiles: {}", Arrays.asList(context.getEnvironment().getActiveProfiles()));
     }
     
 }
